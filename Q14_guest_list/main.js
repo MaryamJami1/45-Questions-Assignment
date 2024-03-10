a8d2b1fe4399d54b06591f126ffa82f88c5b76eb
MaryamJami1 <maryamchouhan373@gmail.com>
2024-03-10 22:50:11 +0500

    .Q14

---
+++ Q14_guest_list/main.js
@@ -0,0 +1,3 @@
+// guest list
+var guestList = ["mahajabeen", "lavishka", "hareem", "sara"];
+guestList.forEach(function (guest) { return console.log("Hello ".concat(guest, " you are invited a dinner!")); });
---
+++ Q14_guest_list/main.ts
@@ -0,0 +1,5 @@
+// guest list
+
+let guestList = ["mahajabeen", "lavishka", "hareem", "sara"];
+
+guestList.forEach (guest => console.log(`Hello ${guest} you are invited a dinner!`));
