de723315a0381dce69f0e74905c31d4fbe4b3c20
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 22:44:38 +0500

    question-20complete

---
+++ question-20/app.js
@@ -0,0 +1,6 @@
+//Making a Programing Language Array
+var favouriteSubjects = ["Science", "Computer", "Urdu", "English"];
+//Print the message of list
+console.log("List of Favourite Subjects:");
+//Print the values of Array in the form of list
+favouriteSubjects.forEach(function (subject) { return console.log(subject); });
---
+++ question-20/app.ts
@@ -0,0 +1,8 @@
+//Making a Programing Language Array
+let favouriteSubjects: string[] = ["Science", "Computer","Urdu", "English"];
+
+//Print the message of list
+console.log("List of Favourite Subjects:");
+
+//Print the values of Array in the form of list
+favouriteSubjects.forEach(subject => console.log(subject));
