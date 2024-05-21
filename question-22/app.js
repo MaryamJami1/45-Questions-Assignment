0da9ee22d004c54f32e0c453d454cc5377cf32d0
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 22:54:38 +0500

    add question-22

---
+++ question-22/app.js
@@ -0,0 +1,6 @@
+//creating a Array 
+var errorArray = ["A", "B", "C", "D"];
+// Producing Error ! by Accessing invalid index of Array
+// console.log(errorArray[10]);
+//Error Remove
+console.log(errorArray[1]);
---
+++ question-22/app.ts
@@ -0,0 +1,8 @@
+//creating a Array 
+let errorArray: string[] = ["A", "B", "C", "D"];
+
+// Producing Error ! by Accessing invalid index of Array
+// console.log(errorArray[10]);
+
+//Error Remove
+console.log(errorArray[1]);
