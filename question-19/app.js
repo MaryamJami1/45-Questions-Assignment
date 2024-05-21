774ba2f6599eb926d9c54f3cd9b6f169618eba5e
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 22:38:59 +0500

    complete q-19

---
+++ question-19/app.js
@@ -0,0 +1,4 @@
+var guestList = ["Aqsa", "Hareem", "Maha", "Hussain", "Moosa"];
+//guestList.forEach(oneGuest => console.log(`Assalam o alaikum ${oneGuest}, would you like to lunch with me?`));
+var lengthGuests = guestList.length;
+console.log("We are inviting total ".concat(lengthGuests, " guests."));
---
+++ question-19/app.ts
@@ -0,0 +1,7 @@
+let guestList = ["Aqsa","Hareem","Maha","Hussain", "Moosa"];
+
+//guestList.forEach(oneGuest => console.log(`Assalam o alaikum ${oneGuest}, would you like to lunch with me?`));
+
+let lengthGuests: number = guestList.length;
+
+console.log(`We are inviting total ${lengthGuests} guests.`);
