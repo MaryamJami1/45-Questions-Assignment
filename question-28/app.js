6208af174193003a1fce7b6931cad99a579a005f
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 23:21:53 +0500

    complete q-28

---
+++ question-28/app.js
@@ -0,0 +1,21 @@
+//Creating a VAriable
+var age = 20;
+//Creating a program for Determining the syage of life using the If-Else chain
+if (age < 2) {
+    console.log("That the person is Baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("That the person is Toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("That the person is kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("That the person is Teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("That the person is Adult.");
+}
+else if (age >= 65) {
+    console.log("That the person is Elder.");
+}
