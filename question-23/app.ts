5f3aab4790f6e3bb0a72755fe84a2b3cf7b81287
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 22:59:50 +0500

    complete question-23

---
+++ question-23/app.ts
@@ -0,0 +1,44 @@
+//Making A Variable
+let eight = 8;
+
+let four = 4;
+
+//Test 1
+console.log("Test 1: eight is equal to 8?");
+console.log(eight == 8);
+
+//Test 2
+console.log("\nTest 2: four is Equal to 4?");
+console.log(four == 4);
+
+//Test 3
+console.log("\nTest 3: 8 is not equal to 4?");
+console.log(eight != 4);
+
+//Test 4
+console.log("\nTest 4: 4 is greater than 8?");
+console.log(four > 8);
+
+//Test 8
+console.log("\nTest 8: 8 is smaller than 4?");
+console.log(8 < 4);
+
+// Test 6
+console.log("\nTest 6: 4 is Smaller than 8?");
+console.log(4 < 8);
+
+//Test 7
+console.log("\nTest 7: 8 is Greater than 4?");
+console.log(8 > 4);
+
+//Test 8
+console.log("\nTest 8: 8 is Equal to 4");
+console.log(eight == four);
+
+//Test 9
+console.log("\nTest 9: 4 is not Equal to 4?");
+console.log(four != 4);
+ 
+//Test 4
+console.log("\nTest 4: 8 is not Equal to 8?");
+console.log(eight != 8);
