67c1fd95def21f45dcf48c275df2544f6cb67966
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 22:50:43 +0500

    complete question-21

---
+++ question-21/app.js
@@ -0,0 +1,8 @@
+var itCourse = {
+    governorName: "Kamran Tessori",
+    courseName: "Typrscript and Javascript",
+    location: "Governor House Sindh",
+    onSiteStudents: 50000,
+    areYouEnrolled: true
+};
+console.log(itCourse);
---
+++ question-21/app.ts
@@ -0,0 +1,17 @@
+interface itCourse {
+    governorName:string,
+    courseName: string,
+    location: string,
+    onSiteStudents: number,
+    areYouEnrolled:boolean,
+}
+
+let itCourse = {
+    governorName:"Kamran Tessori",
+    courseName: "Typrscript and Javascript",
+    location: "Governor House Sindh",
+    onSiteStudents: 50000,
+    areYouEnrolled:true
+};
+
+console.log(itCourse);
