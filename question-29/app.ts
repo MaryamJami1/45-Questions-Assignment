b4956eb9879e89e005a5bd1b035ff1745cf2331a
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 23:27:57 +0500

    complete q-29

---
+++ question-29/app.ts
@@ -0,0 +1,21 @@
+//Creatina a array
+let favouriteFruits = ["Orange", "Guava", "Strawberry"];
+
+
+//using 5 Independent If statements
+
+if (favouriteFruits.includes("Orange")) {
+    console.log("I really like Orange.");
+}
+if (favouriteFruits.includes("Guava")) {
+    console.log("I really like Guava.");
+}
+if (favouriteFruits.includes("Mango")) {
+    console.log("I dont like Mango.");
+}
+if (favouriteFruits.includes("Strawberry")) {
+    console.log("I really like Strawberry.");
+}
+if (favouriteFruits.includes("Banana")) {
+    console.log("I dont like Banana.");
+}
