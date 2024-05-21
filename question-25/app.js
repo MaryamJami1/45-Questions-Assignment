14c81f4c67c7b7ed5d0e992056bca0e6b461d396
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 23:09:00 +0500

    complete q-25

---
+++ question-25/app.js
@@ -0,0 +1,8 @@
+var alienColor = "green";
+if (alienColor === "green") {
+    //code
+    console.log("Alien color is green player just earned 5 points");
+}
+if (alienColor === "White") {
+    console.log("Alien color is White");
+}
---
+++ question-25/app.ts
@@ -0,0 +1,10 @@
+let alienColor = "green";
+
+if (alienColor === "green") {
+    //code
+    console.log("Alien color is green player just earned 5 points");
+}
+
+if (alienColor === "White"){
+    console.log("Alien color is White");
+}
