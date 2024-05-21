7bfe8155ca2627983bbec6d5559220af51bdb48c
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 23:18:12 +0500

    complete q-27

---
+++ question-27/app.ts
@@ -0,0 +1,40 @@
+//Define Variable
+let alienColor = "Red";
+
+//using If and Else_If Statement
+//First version
+if (alienColor === "Red"){
+console.log("Version 1: You shoot down Red Alien you have earned 5 points");
+}
+else if (alienColor === "Green"){
+    console.log("Version 1: you shoot down Green Alien you have earne 10 points");
+}
+else if (alienColor === "Pink"){
+    console.log("Version 1: yOu shoot down Pink Alien you have earned 15 points");
+}
+
+//Second Version
+let alienColor2 = "Green";
+
+if (alienColor2 === "Red"){
+    console.log("Version 2: You shoot down Red Alien you have earned 5 points");
+    }
+    else if (alienColor2 === "Green"){
+        console.log("Version 2: you shoot down Green Alien you have earne 10 points");
+    }
+    else if (alienColor2 === "Pink"){
+        console.log("Version 2: yOu shoot down Pink Alien you have earned 15 points");
+    }
+
+    //Third Version
+let alienColor3 = "Pink";
+
+if (alienColor3 === "Green"){
+    console.log("Version 3: You shoot down Red Alien you have earned 5 points");
+    }
+    else if (alienColor3 === "Green"){
+        console.log("Version 3: you shoot down Green Alien you have earned 10 points");
+    }
+    else if (alienColor3 === "Pink"){
+        console.log("Version 3: yOu shoot down Pink Alien you have earned 15 points");
+    }
