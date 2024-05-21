152323aa58875966c16bde05b420e41cf4ae3fe8
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-21 23:13:22 +0500

    complete q-26

---
+++ question-26/app.js
@@ -0,0 +1,16 @@
+//Define Variable
+var alienColor = "Orange";
+//First Version
+if (alienColor === "Orange") {
+    console.log("\nPlayer just earned 10 points for shooting the Orange alien");
+}
+else {
+    console.log("Shooting alien is not Orange so Player just earned 5 points");
+}
+//Second Version 
+if (alienColor === "Purple") {
+    console.log("I am come from If Statement");
+}
+else {
+    console.log("\nI am come from Else statement");
+}
---
+++ question-26/app.ts
@@ -0,0 +1,21 @@
+//Define Variable
+let alienColor = "Orange";
+
+//First Version
+
+if (alienColor === "Orange") {
+    console.log("\nPlayer just earned 10 points for shooting the Orange alien");
+}
+else {
+    console.log("Shooting alien is not Orange so Player just earned 5 points");
+
+}
+
+//Second Version 
+
+if (alienColor === "Purple") {
+    console.log("I am come from If Statement");
+}
+else {
+    console.log("\nI am come from Else statement");
+}
