1a962172a278b3127f11aba9f929545ac9b9d75c
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-24 16:32:49 +0500

    complete question-39

---
+++ question-39/app.js
@@ -0,0 +1,8 @@
+//Creating a function with ers which return a values in string
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+//Calling a Function & print the return value
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Berlin", "Germany"));
+console.log(city_country("Tokyo", "Japan"));
---
+++ question-39/app.ts
@@ -0,0 +1,13 @@
+//Creating a function with ers which return a values in string
+function city_country(city: string, country: string) : string{
+    return `${city}, ${country}`;
+}
+
+//Calling a Function & print the return value
+
+console.log(city_country("Karachi", "Pakistan"));
+
+console.log(city_country("Berlin", "Germany"));
+
+console.log(city_country("Tokyo", "Japan"));
+
