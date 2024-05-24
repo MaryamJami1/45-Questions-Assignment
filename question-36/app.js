8bb0bee7c5dffb2b021258b6afb78b73cee2cede
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-24 16:20:48 +0500

    complete question-36

---
+++ question-36/app.js
@@ -0,0 +1,6 @@
+//question-36
+function makeShirt(size, printMessage) {
+    console.log("you Selected ".concat(size, " size Shirt with ").concat(printMessage, " Prints on Shirt"));
+}
+makeShirt("Medium", "Code with Maryam");
+makeShirt("Small", "Maryam Mern Stack Developer");
---
+++ question-36/app.ts
@@ -0,0 +1,9 @@
+//question-36
+
+function makeShirt (size: string, printMessage: string){
+    console.log(`you Selected ${size} size Shirt with ${printMessage} Prints on Shirt`);
+}
+
+makeShirt("Medium", "Code with Maryam");
+
+makeShirt("Small", "Maryam Mern Stack Developer");
